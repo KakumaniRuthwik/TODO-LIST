57d20c5cf64bcbcc459cf28422b7721d39685533
KakumaniRuthwik <ruthwikkakumani08@gmail.com>
2025-03-24 14:05:54 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',  // Enables JIT mode
+    content: ["./src/**/*.{html,js}"], // Scan only required files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
